6417223694937613d3c52cb3df225cee03fef78f
Imam Asari <imam.as@treeclouds.com>
2022-01-30 10:39:01 +0700

    implement on message react native webview

--- App.js
+++ App.js
@@ -1,64 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import type { Node } from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  useColorScheme,
+  Dimensions,
+  Text
 } from 'react-native';
-
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
 
 import { WebView } from 'react-native-webview';
 
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <>
+      <Text style={{textAlign: 'center', fontSize: 20}}>Dacast Player</Text>
       <WebView
-        source={{ uri: 'https://netpolitan-eval.percipio.com/courses/6b68cf67-439e-477f-9bdd-3d5e3df91b15?preventAppDownload=true&chromeless=true' }}
+        source={{ uri: 'https://agitated-chandrasekhar-e6ed92.netlify.app' }}
         style={{
-          marginTop: 20,
           width: Dimensions.get('window').width,
           flex: 1
         }}
+        onMessage={(event) => {
+          alert(event.nativeEvent.data);
+        }}
       />
-    </SafeAreaView>
+    </>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
